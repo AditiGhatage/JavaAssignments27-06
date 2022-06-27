6be532d42ee90ae690dbe36e3e08f2ac4a5a66b8
Aditi Ghatage <104888687+AditiGhatage@users.noreply.github.com>
2022-06-27 19:21:05 +0530

    Add files via upload

--- SearchArrayElements.java
+++ SearchArrayElements.java
@@ -2,5 +2,30 @@ package com.assignments;
 
 public class SearchArrayElements 
 {
+	public void searchCommonElements(int intarray1[], int intarray2[], int intarray3[])
+	{
+		int intx = 0, inty = 0, intz = 0;
+		System.out.println("Common elements are:");
+		while(intx < intarray1.length && inty < intarray2.length && intz < intarray3.length) { 
+			 
+            if(intarray1[intx] == intarray2[inty] && intarray2[inty] == intarray3[intz]) 
+            { 
+            	
+                  System.out.println(intarray1[intx]); 
+                  	intx++; 
+                  	inty++; 
+                  	intz++; 
+            } else if (intarray1[intx] > intarray2[inty]) {
+                   inty++;
+
+             } else if (intarray2[inty] > intarray3[intz]) {
+            	 intz++;
+
+             } else {
+            	 intx++;
+             }
+         }
+		
+	}
 
 }
--- TestSearchArrayElements.java
+++ TestSearchArrayElements.java
@@ -2,8 +2,14 @@ package com.assignments;
 
 public class TestSearchArrayElements {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		SearchArrayElements objSearchArrayElements = new SearchArrayElements();
+		int intarray1[] = {1,4,6,8,9};
+		int intarray2[] = {1,3,4,8};
+		int intarray3[] = {2,4,7,5,8,8,9};
+		
+		objSearchArrayElements.searchCommonElements(intarray1, intarray2, intarray3);
 
 	}
 
